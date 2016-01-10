bf530fdc3bf09e70c56194a05eaa30fb7865baf8
Mehmet Goren <mehmetgoren@outlook.com>
2016-01-10 14:21:51 +0200

    1

---
+++ src/ionix/Conversion/Convertible.java
@@ -0,0 +1,33 @@
+package ionix.Conversion;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public interface Convertible {
+
+    boolean toBoolean(Object value);
+
+    char toChar(Object value);
+
+    byte toByte(Object value);
+
+    short toShort(Object value);
+
+    int toInt(Object value);
+
+    long toLong(Object value);
+
+    float toFloat(Object value);
+
+    double toDouble(Object value);
+
+    Date toDate(Object value);
+
+    String toString(Object value);
+
+    BigDecimal toBigDecimal(Object value);
+
+    byte[] toByteArray(Object value);
+
+    UUID toUUID(Object value);
+}
